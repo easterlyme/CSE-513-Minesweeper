e4c8de459aa7b613060b6a3f03da67dbe9273432
Jacques <breauxjaqjr@comcast.net>
2017-03-22 12:40:09 -0400

    Incorrect implementation, keeps getting NaN

--- src/vote/VoteStrategy.java
+++ src/vote/VoteStrategy.java
@@ -7,6 +7,7 @@ package vote;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import map.Strategy;
 import map.Map;
 
@@ -16,16 +17,18 @@ public final class VoteStrategy implements Strategy {
 	private int height;
 
 	private ArrayList<Tile> fringe;
-	private int[] votes;
+	private ArrayList<Tile> voteFringe;
+	private Tile[][] board;
 
 	void initialize(Map m) {
 		width = m.columns();
 		height = m.rows();
 		fringe = new ArrayList<Tile>();
-		votes = new int[height * width];
+		voteFringe = new ArrayList<Tile>();
+		board = new Tile[width][height];
 		for (int i = 0; i < width; i++) {
 			for(int j = 0; j < height; j++) {
-				votes[i * height + j] = 1000;
+				board[i][j] = new Tile(i,j);
 			}
 		}
 	}
@@ -33,12 +36,7 @@ public final class VoteStrategy implements Strategy {
 	void ChooseRandom(Map m) {
 		int x = m.pick(width);
 		int y = m.pick(height);
-		int q = m.probe(x,y);
-		if(q == 0) {
-			RevealSurrounding(x,y,m);	
-		} else {
-			fringe.add(new Tile(x,y));
-		}
+		Reveal(x,y,m);
 	}
 
 	void RevealSurrounding(int x,int y, Map m) {
@@ -50,18 +48,71 @@ public final class VoteStrategy implements Strategy {
 				}
 				q = m.look(i,j);
 				if(q == Map.UNPROBED) {
-					q = m.probe(i,j);
-					if(q == 0) {
-						RevealSurrounding(i,j,m);
-					} else {
-						fringe.add(new Tile(i,j));
+					Reveal(i,j,m);
+				}
+			}
+		}
+	}
+
+	void Reveal(int x, int y, Map m) {
+		int q = m.probe(x,y);
+		if(q == 0) {
+			RevealSurrounding(x,y,m);
+		} else {
+			voteFringe.remove(board[x][y]);
+			fringe.add(board[x][y]);
+			for(int i = x - 1; i <= x + 1; i++) {
+				for(int j = y - 1; j <= y+1; j++) {
+					if(m.look(i,j) == Map.UNPROBED && !voteFringe.contains(board[i][j])) {
+						voteFringe.add(board[i][j]);
 					}
 				}
 			}
 		}
 	}
 
-	void Vote() {
+	void VotePhase(Map m) {
+		for(Tile t: voteFringe) {
+			t.Reset();//Reset for votes
+		}
+		Iterator<Tile> iFringe = fringe.iterator();
+		while (iFringe.hasNext()) {
+			Tile t = iFringe.next();
+			int i = t.x - 1 < 0 ? 0 : t.x - 1;
+			int j = t.y - 1 < 0 ? 0 : t.y - 1;
+			int x = i;
+			int y = j;
+			int unprobed = 0;
+			for(;i < t.x + 1 && i < width; i++) {
+				for(;j < t.x + 1 && j < height; j++) {
+					if(m.look(i,j) == Map.UNPROBED){
+						unprobed++;//counting unprobed neighbors
+					}
+				}
+			}
+			if(unprobed == 0) { //remove from fringe, it's done
+				iFringe.remove();
+				continue;
+			}
+			double score = 1 - m.look(t.x,t.y) / unprobed;
+			for(;x < t.x + 1 && x < width; x++) {
+				for(;y < t.y + 1 && y < height; y++) {
+					if(m.look(x,y) == Map.UNPROBED){
+						board[x][y].Vote(score);
+					}
+				}
+			}
+		}
+	}
+
+	Tile ChooseBest() {
+		Tile out = voteFringe.get(0);
+		for(Tile t: voteFringe) {
+			if(out.getScore() < t.getScore()) {
+				out = t;
+			}
+		}
+		return out;
 	}
 
 	/**
@@ -72,17 +123,45 @@ public final class VoteStrategy implements Strategy {
 		initialize(m);
 		ChooseRandom(m);
 		while(!m.done()) {
-			
-			ChooseRandom(m);
+			VotePhase(m);
+			Tile guess = ChooseBest();
+			System.out.println(guess.getScore());
+			if(guess.getScore() < 0.2) {
+				ChooseRandom(m);
+			} else {
+				//Remove from votefringe
+				voteFringe.remove(guess);
+				Reveal(guess.x, guess.y, m);
+			}
 		}
+		for(Tile t: voteFringe)
+			System.out.print(t.getScore() + " ");
 	}
 
 	class Tile {
 		int x;
 		int y;
+		private int votes;
+		private double score;
 		Tile(int xPos, int yPos) {
 			x = xPos;
 			y = yPos;
+			votes = 0;
+			score = 0;
+		}
+
+		void Vote(double vote) {
+			votes++;
+			score += vote;
+		}
+		
+		public double getScore() {
+			return score/votes;
+		}
+
+		void Reset() {
+			votes = 0;
+			score = 0;
 		}
 	}
 }
